71433a6d51b435f0bc0b1808fdd8bed30c3f8ca9
Tyler Kellen <tyler@sleekcode.net>
2015-05-19 17:26:49 -0400

    streamline support for fallback modules and register configuration

--- index.js
+++ index.js
@@ -1,65 +1,58 @@
-var extensions = {
-  '.babel.js': 'babel/register',
+const extensions = {
+  '.babel.js': {
+    module: 'babel/register',
+    register: function (module) {
+      module({
+        // register on .js extension due to https://github.com/joyent/node/blob/v0.12.0/lib/module.js#L353
+        // which only captures the final extension (.babel.js -> .js)
+        extensions: '.js'
+      })
+    }
+  },
   '.cirru': 'cirru-script/lib/register',
   '.cjsx': 'node-cjsx/register',
   '.co': 'coco',
-  '.coffee': 'coffee-script/register',
-  '.coffee.md': 'coffee-script/register',
+  '.coffee': ['coffee-script/register', 'coffee-script'],
+  '.coffee.md': ['coffee-script/register', 'coffee-script'],
   '.csv': 'require-csv',
-  '.iced': 'iced-coffee-script/register',
-  '.iced.md': 'iced-coffee-script/register',
+  '.iced': ['iced-coffee-script/register', 'iced-coffee-script'],
+  '.iced.md': ['iced-coffee-script/register', 'iced-coffee-script'],
   '.ini': 'require-ini',
   '.js': null,
   '.json': null,
   '.json5': 'json5/lib/require',
-  '.jsx': 'node-jsx',
-  '.litcoffee': 'coffee-script/register',
-  '.liticed': 'iced-coffee-script/register',
-  '.ls': 'LiveScript',
+  '.jsx': [
+    {
+      module: 'babel/register',
+      register: function (module) {
+        module({
+          extensions: '.jsx'
+        });
+      },
+    },
+    {
+      module: 'node-jsx',
+      register: function (module) {
+        module.install({
+          extension: '.jsx',
+          harmony: true
+        });
+      }
+    }
+  ],
+  '.litcoffee': ['coffee-script/register', 'coffee-script'],
+  '.liticed': ['iced-coffee-script/register', 'iced-coffee-script'],
+  '.ls': ['livescript', 'LiveScript'],
   '.node': null,
   '.toml': 'toml-require',
-  '.ts': 'typescript-register',
+  '.ts': ['typescript-register', 'typescript-require'],
   '.wisp': 'wisp/engine/node',
   '.xml': 'require-xml',
   '.yaml': 'require-yaml',
   '.yml': 'require-yaml'
 };
 
-var register = {
-  'babel/register': function (module, config) {
-    module(config);
-  },
-  'node-jsx': function (module, config) {
-    module.install(config);
-  },
-  'toml-require': function (module) {
-    module.install();
-  }
-};
-
-var legacyModules = {
-  '.coffee': 'coffee-script',
-  '.coffee.md': 'coffee-script',
-  '.iced': 'iced-coffee-script',
-  // .iced.md and .liticed weren't available before the register module
-  '.litcoffee': 'coffee-script',
-  // typescript-require is for versions of TypeScript before 1.4
-  '.ts': 'typescript-require'
-};
-
-var configurations = {
-  'babel/register': {
-    // register on .js extension due to https://github.com/joyent/node/blob/v0.12.0/lib/module.js#L353
-    // which only captures the final extension (.babel.js -> .js)
-    extensions: '.js'
-  },
-  'node-jsx': {
-    extension: '.jsx',
-    harmony: true
-  }
-};
-
-var jsVariantExtensions = [
+const jsVariantExtensions = [
   '.js',
   '.babel.js',
   '.cirru',
@@ -79,9 +72,6 @@ var jsVariantExtensions = [
 
 module.exports = {
   extensions: extensions,
-  legacy: legacyModules,
-  configurations: configurations,
-  register: register,
   jsVariants: jsVariantExtensions.reduce(function (result, ext) {
     result[ext] = extensions[ext];
     return result;
