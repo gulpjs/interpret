8741ac8d2b332ca987422910b04f801dc9fe9d7d
Blaine Bublitz <blaine.bublitz@gmail.com>
2022-04-11 17:32:42 -0700

    formatting

--- test/fixtures/ts/2/test.ts
+++ test/fixtures/ts/2/test.ts
@@ -1,19 +1,19 @@
 var test: {
-    data: {
-        trueKey: boolean;
-        falseKey: boolean;
-        subKey: {
-            subProp: number;
-        };
+  data: {
+    trueKey: boolean;
+    falseKey: boolean;
+    subKey: {
+      subProp: number;
     };
+  };
 } = {
-    data: {
-        trueKey: true,
-        falseKey: false,
-        subKey: {
-            subProp: 1,
-        },
+  data: {
+    trueKey: true,
+    falseKey: false,
+    subKey: {
+      subProp: 1,
     },
+  },
 };
 
 export default test;
